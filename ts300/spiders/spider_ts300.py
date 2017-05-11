1c4f90bc1e8264e602ca5cae4748aea2a1d063ac
王耀诚 <yocson@outlook.com>
2017-05-11 21:14:32 +0800

    正确爬取所有诗文 & 优化代码

--- ts300/spiders/spider_ts300.py
+++ ts300/spiders/spider_ts300.py
@@ -15,7 +15,6 @@ class Ts300Spider(scrapy.Spider):
         links = []
         for element in response.xpath("//*[@class='guwencont2']/a"):
             links.append(element.xpath("@href").extract()[0])
-            
         for poem_url in links:
             yield scrapy.Request("http://www.gushiwen.org"+poem_url, self.parse_poem)
 
@@ -23,12 +22,30 @@ class Ts300Spider(scrapy.Spider):
         item = Ts300Item()
         item['author'] = response.xpath("/html/body/div[3]/div[3]/div[2]/div[1]/div[1]/a/strong/text()").extract()[0]
         item['title'] = response.xpath("/html/body/div[3]/div[3]/div[2]/div[2]/div[1]/h1/text()").extract()[0]
-        poemstr = ''
-        poemlist= response.css("p[align='center']").extract()
-        if poemlist == []:
-            poemstr = response.xpath("/html/body/div[3]/div[3]/div[2]/div[2]/div[2]/p[1]").extract()[0]
-        else:
-            for sen in poemlist:
-                poemstr += sen
-        item['poem'] = poemstr
+        item['poem'] = get_poem(item['title'], response)
         yield item
+
+
+def append_poemstr(poemlist, num):
+    poemstr = ''
+    for sen in poemlist[0:num]:
+        poemstr += sen
+    return poemstr
+
+def get_poem(title, response):
+    poemstr = ''
+    poemlist = response.css("p[align='center']").extract()
+    if poemlist == []:
+        poemstr = response.xpath("/html/body/div[3]/div[3]/div[2]/div[2]/div[2]/p[1]").extract()[0]
+        exception_dict = {
+            '宣州谢朓楼饯别校书叔云' : 6,
+            '行路难·大道如青天' : 8
+        }
+        title = title.encode("utf8")
+        if exception_dict.get(title):
+            poemlist = response.xpath("/html/body/div[3]/div[3]/div[2]/div[2]/div[2]/p").extract()
+            poemstr = append_poemstr(poemlist, exception_dict.get(title))
+    else:
+        poemstr = append_poemstr(poemlist, len(poemlist))
+    return poemstr
+        
